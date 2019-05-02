b06619c8a4128d4d24bb15921418454997c3f427
IAmRDhar <rahul.dhar47@gmail.com>
2019-05-02 23:30:52 +0530

    Added load balancer

---
+++ backend/loadbalancer/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type webRequest struct {
+	r *http.Request
+	w http.ResponseWriter
+	// lets us know when the
+	// request processor is done
+	doneCh chan struct{}
+}
+
+var (
+	requestCh = make(chan *webRequest)
+)
+
+var (
+	transport = http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+)
+
+func init() {
+	http.DefaultClient = &http.Client{Transport: &transport}
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// handling requests off to a
+		// request processor asynchronously
+		doneCh := make(chan struct{})
+		requestCh <- &webRequest{
+			r:      r,
+			w:      w,
+			doneCh: doneCh,
+		}
+
+		<-doneCh
+
+	})
+
+	go processRequests()
+
+	go http.ListenAndServeTLS(":2000", "/cert.pem", "/key.pem", nil)
+
+	log.Println("Server started, press <ENTER> to exit")
+	fmt.Scanln()
+
+}
