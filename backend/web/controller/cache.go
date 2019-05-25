469488bc7b3e875310e9fbe99133f101ee8dcc2c
IAmRDhar <rahul.dhar47@gmail.com>
2019-05-25 19:47:20 +0530

    Cache controller for web app

---
+++ backend/web/controller/cache.go
@@ -0,0 +1,30 @@
+package controller
+
+import (
+	"bytes"
+	"flag"
+	"io"
+	"net/http"
+	"strconv"
+)
+
+var cacheServiceURL = flag.String("cachingservice", "https://172.18.0.13:5000", "Address of the caching service")
+
+func getFromCache(key string) (io.ReadCloser, bool) {
+	resp, err := http.Get(*cacheServiceURL + "/?key=" + key)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		println("get fail")
+		return nil, false
+	}
+	return resp.Body, true
+}
+
+func savingToCache(key string, duration int64, data []byte) {
+	req, _ := http.NewRequest(http.MethodPost, *cacheServiceURL+"/?key="+key, bytes.NewBuffer(data))
+	req.Header.Add("cache-control", "maxage="+strconv.FormatInt(duration, 10))
+	http.DefaultClient.Do(req)
+}
+
+func invalideCacheEntry(key string) {
+	http.Get(*cacheServiceURL + "/invalidate?key=" + key)
+}
