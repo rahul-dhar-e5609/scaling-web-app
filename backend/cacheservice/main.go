b8762aeb2c15eeecd7a642e405a61526e284d9cc
IAmRDhar <rahul.dhar47@gmail.com>
2019-05-25 19:21:44 +0530

    Add caching service

---
+++ backend/cacheservice/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	data       []byte
+	expiration time.Time
+}
+
+var (
+	cache = make(map[string]*cacheEntry)
+	mutex = sync.RWMutex{}
+)
+
+var maxAgeRegexp = regexp.MustCompile(`maxage=(\d+)`)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			getFromCache(w, r)
+		} else if r.Method == http.MethodPost {
+			saveToCache(w, r)
+		}
+	})
+
+	go http.ListenAndServeTLS(":5000", "/cert.pem", "/key.pem", nil)
+
+	log.Println("Caching service started, press <ENTER> to exit")
+
+	fmt.Scanln()
+}
+
+func getFromCache(w http.ResponseWriter, r *http.Request) {
+	mutex.RLock()
+	defer mutex.RUnlock()
+
+	key := r.URL.Query().Get("key")
+	fmt.Printf("Searching cahce for %s...\n", key)
+	if entry, ok := cache[key]; ok {
+		fmt.Println("Found")
+		w.Write(entry.data)
+		return
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Println("Not found")
+}
+
+func saveToCache(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	key := r.URL.Query().Get("key")
+	cacheHeader := r.Header.Get("cache-control")
+
+	fmt.Printf("Saving cache entry with key %s for %s seconds\n", key, cacheHeader)
+
+	matches := maxAgeRegexp.FindStringSubmatch(cacheHeader)
+	if len(matches) == 2 {
+		dur, _ := strconv.Atoi(matches[1])
+		data, _ := ioutil.ReadAll(r.Body)
+		cache[key] = &cacheEntry{data: data, expiration: time.Now().Add(time.Duration(dur) * time.Second)}
+	}
+}
